47ef3ccaa6c511d0da10a274427e8f2b372cb92a
Scott Greenup <scott.j.greenup@gmail.com>
2015-04-08 16:47:12 +1000

    Finished 50A.

---
+++ 50A/main.cc
@@ -0,0 +1,15 @@
+// m * n squares
+// dominoes are 2x1
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int m, n;
+    cin >> m >> n;
+
+    cout << (m * n / 2) << endl;
+
+
+    return 0;
+}
